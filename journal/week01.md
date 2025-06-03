015389b06eb0805e42db8279aaf0650a5cfb0413
mjolsen-sw <mjolsen.sw@gmail.com>
2025-06-03 10:34:07 -0700

    initial kubernetes deployment

--- journal/week01.md
+++ journal/week01.md
@@ -14,3 +14,27 @@ pip install flask
 pip install psycopg2-binary
 deactivate
 ```
+## Kubernetes
+The auth service's Kubernets configs are located in `python/src/auth/manifests`.
+They're currently set to pull CONNECTION_URL and JWT_SECRET from AWS SSM Paramter Store.
+### Generate ECR credentials secret
+We'll be needing to install go and helm for this process.
+After, to be able to use external secrets:
+```sh
+kubectl apply -f https://raw.githubusercontent.com/external-secrets/external-secrets/main/deploy/crds/bundle.yaml
+helm repo add external-secrets https://charts.external-secrets.io
+helm install external-secrets external-secrets/external-secrets \
+  -n external-secrets --create-namespace \
+  --set installCRDs=false
+```
+To store ECR credentials to be able to pull image from ECR:
+```sh
+kubectl create secret docker-registry ecr-creds \
+  --docker-server=009160064216.dkr.ecr.us-west-1.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password="$(aws ecr get-login-password --region us-west-1)"
+```
+In `python/src/auth/manifests/`:
+```sh
+kubectl apply -f ./
+```
--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -35,3 +35,34 @@ def login():
       return createJWT(auth.username, os.getenv('JWT_SECRET'), True)
   else:
     return {'message': 'Invalid credentials'}, 401
+  
+@server.route('/validate', methods=['POST'])
+def validate():
+  encoded_jwt = request.headers.get('Authorization')
+
+  if not encoded_jwt or not encoded_jwt.startswith('Bearer '):
+    return {'message': 'Missing or invalid token'}, 401
+  
+  encoded_jwt = encoded_jwt.split(' ')[1]
+  try:
+    decoded_jwt = jwt.decode(encoded_jwt, os.getenv('JWT_SECRET'), algorithms=['HS256'])
+    return decoded_jwt, 200
+  except jwt.ExpiredSignatureError:
+    return {'message': 'Token has expired'}, 401
+  except jwt.InvalidTokenError:
+    return {'message': 'Invalid token'}, 401
+  
+def createJWT(username, secret, is_admin):
+  return jwt.encode(
+    {
+      'username': username,
+      'exp': datetime.datetime.utcnow(tz=datetime.timezone.utc) + datetime.timedelta(days=1)
+      'iat': datetime.datetime.utcnow(tz=datetime.timezone.utc),
+      'admin': is_admin
+    },
+    secret,
+    algorithm='HS256'
+  )
+
+if __name__ == '__main__':
+  server.run(host='0.0.0.0', port=5000, debug=True)
